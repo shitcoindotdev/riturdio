cc941c5494a3579652eb487b371437d476797808
shitcoindotdev <boomer@shitcoin.dev>
2024-07-12 23:10:38 +0200

    ritardio

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import ritardio from "/public/riturdio.png"
+import riturdio from "/public/riturdio.gif"
+import Links from "@/components/Links";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`bg-mario-colors flex min-h-screen w-full flex-col gap-8 items-center justify-center p-8 ${inter.className}`}
+    >
+    <Image className="w-[69vw] shiver max-w-[500px] h-auto" src={ritardio} width={0} height={0} alt="riturdio" />
+    <Image className="w-[40vw] rounded-xl max-w-[420px] h-auto" src={riturdio} width={0} height={0} alt="riturdio" />
+    <Links />
+    </main>
+  );
+}
