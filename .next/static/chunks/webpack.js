7fd77ac809a18ff97d7a69071c945e7f6a282f99
shitcoindotdev <boomer@shitcoin.dev>
2024-07-31 14:15:37 +0200

    mdemd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6b5485b1ddc5ceb7"; }
+/******/ 		__webpack_require__.h = function() { return "f965b2ff459d8163"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,21 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import ritardio from '/public/riturdio.png'
-import riturdio from '/public/riturdio.gif'
 import Links from '@/components/Links'
+import CopyCa from '@/components/copyCa'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 	return (
 		<main
-			className={`bg-mario-colors flex min-h-screen w-full flex-col items-center gap-8 p-8 md:justify-center ${inter.className}`}
+			className={`flex min-h-screen w-full flex-col items-center gap-8 p-8 md:justify-center ${inter.className}`}
 		>
-			<Image
-				className="shiver h-auto w-[69vw] max-w-[500px]"
-				src={ritardio}
-				width={0}
-				height={0}
-				alt="riturdio"
-			/>
-			<Image
-				className="h-auto shiver  w-[80vw] max-w-[420px] rounded-xl"
-				src={riturdio}
-				width={0}
-				height={0}
-				alt="riturdio"
-			/>
+			<CopyCa contractAddress={'1234testaskdjledkjsleckn'} />
 			<Links />
-			<div className="flex flex-col items-center gap-2">
+			{/* <div className="flex flex-col items-center gap-2">
 				<p className="text-2xl font-bold text-white">riturdio.sol</p>
 				<p className="text-xs text-white">CawA7rHFU1RarXiNpVr3TXLn5dYsjLLL9oFSeaNkjLHU</p>
-			</div>
+			</div> */}
 		</main>
 	)
 }
